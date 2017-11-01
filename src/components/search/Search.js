7ea517a7218151ed51abb3a19468f61913c5a897
chrisjxn <usjacksons@gmail.com>
2017-11-01 17:38:10 -0600

    added checkbox to makes list

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,14 +4,24 @@ import { getAllMakes } from './../../redux/reducer';
 
 class Search extends Component {
 
-    componentDidMount() {        
+    componentDidMount() {
         this.props.getAllMakes();
     }
 
     render() {
+        // need to make this a controlled component so checked status takes value from state. also need to add onChange. need to figure out if this should be in another component and where it should go and how to subscribe it to the data it needs and how to get it back into the main Search Component
         let makesList = this.props.allMakes.map(obj => {
-            return <p key={obj.id}>{obj.make}</p>
+            return (
+                <div key={obj.id}>
+                    <input
+                        name={obj.make}
+                        type="checkbox"
+                    />
+                    {obj.make}
+                </div>
+            )
         })
+
         return (
             <div>
                 This is the Search Component
